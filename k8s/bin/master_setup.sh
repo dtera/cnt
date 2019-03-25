ce900fc183a3718e2c1e28141834eb518435e63f
zhaohq <45172737+dtera@users.noreply.github.com>
2019-03-25 20:38:04 +0800

    Create master_setup.sh

---
+++ k8s/bin/master_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sh node_setup.sh
+
+yum install -y kubectl
+
+kubeadm init --ignore-preflight-errors=Swap --image-repository=registry.aliyuncs.com/google_containers --pod-network-cidr 10.244.0.0/16
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/dtera/cnt/master/k8s/kube-flannel.yml
