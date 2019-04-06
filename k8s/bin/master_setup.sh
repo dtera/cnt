b28d21ce94ef226f8b18fcc98b1529ec4146d391
zhaohq <45172737+dtera@users.noreply.github.com>
2019-04-06 22:47:32 +0800

    Update master_setup.sh

--- k8s/bin/master_setup.sh
+++ k8s/bin/master_setup.sh
@@ -2,7 +2,7 @@
 
 sh node_setup.sh
 
-kubeadm init --ignore-preflight-errors=Swap --image-repository=registry.aliyuncs.com/google_containers --pod-network-cidr 10.244.0.0/16
+kubeadm init --config=kubeadm-config.yml --experimental-upload-certs --ignore-preflight-errors=Swap
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 
