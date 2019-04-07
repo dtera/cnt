4d5b8e087d4d25d815ac62469c8d0fb5fea58102
zhaohq <45172737+dtera@users.noreply.github.com>
2019-04-07 16:42:46 +0800

    Update kube-setup.sh

--- k8s/bin/kube-setup.sh
+++ k8s/bin/kube-setup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 WD=$(cd $(dirname $(dirname $0)); pwd)
 
+show_usage="args: [-t|--node_type]"
+node_type="node"
+
+while getopts ":t:" opt
+do
+  case $opt in
+    t) node_type=$OPTARG;;
+    ?) echo "unknow args"; echo $show_usage; exit 1;;
+  esac
+done
+
 yum install -y yum-utils
 yum-config-manager --add-repo=https://raw.githubusercontent.com/dtera/cnt/master/k8s/repo/docker-ce.repo
 yum-config-manager --add-repo=https://raw.githubusercontent.com/dtera/cnt/master/k8s/repo/kubernetes.repo
@@ -14,7 +25,7 @@ KUBELET_CGROUP_ARGS="--cgroup-driver=$DOCKER_CGROUPS"
 KUBELET_EXTRA_ARGS="--fail-swap-on=false"
 EOF
 
-if [[ $1 == "master" ]]; then
+if [[ $node_type == "master" ]]; then
   kubeadm init --config=$WD/kubeadm-config.yml --experimental-upload-certs --ignore-preflight-errors=Swap|tee $WD/kubeadm-init.log
   mkdir -p $HOME/.kube
   cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
