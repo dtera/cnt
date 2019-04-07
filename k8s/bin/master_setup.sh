c75f672eb6c0e49213c21387efafec5b518e8c21
zhaohq <45172737+dtera@users.noreply.github.com>
2019-04-07 08:55:47 +0800

    Update master_setup.sh

--- k8s/bin/master_setup.sh
+++ k8s/bin/master_setup.sh
@@ -3,7 +3,7 @@
 sh node_setup.sh
 
 workdir=$(cd $(dirname $(dirname $0)); pwd)
-kubeadm init --config=$workdir/kubeadm-config.yml --experimental-upload-certs --ignore-preflight-errors=Swap
+kubeadm init --config=$workdir/kubeadm-config.yml --experimental-upload-certs --ignore-preflight-errors=Swap > $workdir/kubeadm-init.log
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 
