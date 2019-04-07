4570c4af8efa89746a5306491d6a9b9e282cda84
zhaohq <45172737+dtera@users.noreply.github.com>
2019-04-07 12:53:21 +0800

    Update node_setup.sh

--- k8s/bin/node_setup.sh
+++ k8s/bin/node_setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+WD=$(cd $(dirname $(dirname $0)); pwd)
 
 yum install -y yum-utils
 yum-config-manager --add-repo=https://raw.githubusercontent.com/dtera/cnt/master/k8s/repo/docker-ce.repo
@@ -12,3 +13,11 @@ cat <<EOF >/etc/sysconfig/kubelet
 KUBELET_CGROUP_ARGS="--cgroup-driver=$DOCKER_CGROUPS"
 KUBELET_EXTRA_ARGS="--fail-swap-on=false"
 EOF
+
+if [[ $1 == "master" ]]; then
+  kubeadm init --config=$WD/kubeadm-config.yml --experimental-upload-certs --ignore-preflight-errors=Swap|tee $WD/kubeadm-init.log
+  mkdir -p $HOME/.kube
+  cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
+
+  kubectl apply -f https://raw.githubusercontent.com/dtera/cnt/master/k8s/kube-flannel.yml
+fi
