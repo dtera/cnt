5e4c3031d64bb84b2ab85d02966786e7eb4f30f7
zhaohq <45172737+dtera@users.noreply.github.com>
2019-04-07 16:09:44 +0800

    Create kubeadm-reset.sh

---
+++ k8s/bin/kubeadm-reset.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+kubeadm reset -f
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
+systemctl stop kubelet 
+rm -rf /var/lib/cni/
+rm -rf /var/lib/kubelet/*
+rm -rf /etc/cni/
+ifconfig cni0 down
+ifconfig flannel.1 down
+ip link delete cni0
+ip link delete flannel.1
