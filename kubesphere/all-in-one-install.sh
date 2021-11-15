fa6c5d265d546bc7a1a1690acc4636ab5d8ecf81
dtrea <45172737+dtera@users.noreply.github.com>
2021-11-15 11:16:36 +0800

    Update all-in-one-install.sh

--- kubesphere/all-in-one-install.sh
+++ kubesphere/all-in-one-install.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-echo '====================BEGIN install Dependency requirements===================='
-yum install -y socat conntrack ebtables ipset
-echo '====================END install Dependency requirements======================'
-
-echo '=====================BEGIN  download and insall KubeKey======================'
-if [[ ! -d "/opt/kubesphere" ]]; then
- mkdir /opt/kubesphere
-fi
-cd /opt/kubesphere
-export KKZONE=cn
-curl -sfL https://get-kk.kubesphere.io | VERSION=v1.1.1 sh -
-chmod +x kk
-echo '=====================END  download and insall KubeKey========================'
+sh ./kk-config.sh
 
 echo '==========================BEGIN install kubesphere==========================='
 hostnamectl set-hostname ks-admin
@@ -20,5 +8,5 @@ hostnamectl set-hostname ks-admin
 # iptables -A INPUT -p tcp --dport 22 -j ACCEPT 
 # iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
-./kk create cluster --with-kubernetes v1.20.4 --with-kubesphere v3.1.1
+./kk create cluster --with-kubesphere v3.2.0 # --with-kubernetes v1.21.5
 echo '==========================END install kubesphere============================='
