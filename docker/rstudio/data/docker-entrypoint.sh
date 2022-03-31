49e87d6bef8ec47d918575fa50b0b03a859e8139
dterazhao <dterazhao@tencent.com>
2022-04-01 01:56:18 +0800

    [feat]: add rstudio

--- docker/rstudio/data/docker-entrypoint.sh
+++ docker/rstudio/data/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
 
-useradd -m -s /bin/bash rstudio
-# echo rstudio | passwd rstudio --stdin
-echo rstudio:rstudio | chpasswd
-ln -s /data /home/rstudio/data
-cd /home/rstudio || exit
+#useradd -m -s /bin/bash rstudio
+#echo rstudio | passwd rstudio --stdin
+#echo rstudio:rstudio | chpasswd
+#ln -s /data /home/rstudio/data
+#cd /home/rstudio || exit
