4fc55f032bf737e3bf91984953f9bfb719316209
dtera <huster1005@gmail.com>
2022-03-31 19:48:01 +0800

    [feat]: add rstudio-server

--- docker/rstudio/data/docker-entrypoint.sh
+++ docker/rstudio/data/docker-entrypoint.sh
@@ -3,4 +3,6 @@
 useradd -m -s /bin/bash rstudio
 # echo rstudio | passwd rstudio --stdin
 echo rstudio:rstudio | chpasswd
+ln -s /data /home/rstudio/data
+cd /home/rstudio || exit
 rstudio-server start
