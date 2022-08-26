ab77d3c65c8d4c5c9a9858b2dc68631ac73b7aa1
dtera <huster1005@gmail.com>
2022-08-26 15:54:45 +0800

    [feat]: add exe pem

--- kubesphere/kk-config.sh
+++ kubesphere/kk-config.sh
@@ -4,19 +4,20 @@ echo '====================BEGIN install Dependency requirements=================
 yum install -y socat conntrack ebtables ipset
 echo '====================END install Dependency requirements======================'
 
-echo '=====================BEGIN  download and insall KubeKey======================'
+echo '=====================BEGIN  download and install KubeKey======================'
 work_dir="/data/kubesphere"
 
 if [[ ! -d "$work_dir" ]]; then
  mkdir "$work_dir"
 fi
+# shellcheck disable=SC2164
 cd "$work_dir"
 
 export KKZONE=cn
 curl -sfL https://get-kk.kubesphere.io | VERSION=v2.2.1 sh -
 chmod +x kk
 cp kk /usr/local/bin
-echo '=====================END  download and insall KubeKey========================'
+echo '=====================END  download and install KubeKey========================'
 
 echo '=====================BEGIN  generate config file============================='
 kk create config --with-kubesphere v3.3.0 # --with-kubernetes v1.22.10 
