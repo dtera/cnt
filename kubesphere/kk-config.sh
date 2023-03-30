8dfd1861650522000514aa859f8d7ec521523d8a
dtrea <45172737+dtera@users.noreply.github.com>
2023-03-30 15:04:47 +0800

    Update kk-config.sh

--- kubesphere/kk-config.sh
+++ kubesphere/kk-config.sh
@@ -14,11 +14,11 @@ fi
 cd "$work_dir"
 
 export KKZONE=cn
-curl -sfL https://get-kk.kubesphere.io | VERSION=v2.2.1 sh -
+curl -sfL https://get-kk.kubesphere.io | VERSION=v3.0.2 sh -
 chmod +x kk
 cp kk /usr/local/bin
 echo '=====================END  download and install KubeKey========================'
 
 echo '=====================BEGIN  generate config file============================='
-kk create config --with-kubesphere v3.3.0 # --with-kubernetes v1.22.10 
+kk create config --with-kubesphere v3.3.1 # --with-kubernetes v1.22.12
 echo '=====================END  generate config file==============================='
