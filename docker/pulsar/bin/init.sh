7b4b780241cd78816469fdf93a55b367901619d8
dterazhao <dterazhao@tencent.com>
2023-04-21 04:16:33 +0800

    [feat]: add exe pem

--- docker/pulsar/bin/init.sh
+++ docker/pulsar/bin/init.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC2155
-export IP=$(ifconfig|grep "inet "|grep -v 127.0.0.1|tail -1|awk '{ print $2 }')
-export PULSAR_PROMETHEUS_URL=http://"$IP":9090
+set -e
 
-CSRF_TOKEN=$(curl http://127.0.0.1:7750/pulsar-manager/csrf-token)
+dc_exec="docker-compose -f docker-compose.yml exec"
 
-curl \
+$dc_exec pulsar ./bin/pulsar-admin tenants create fl-tenant || true
+$dc_exec pulsar ./bin/pulsar-admin namespaces create fl-tenant/fl-namespace || true
+$dc_exec pulsar ./bin/pulsar-admin namespaces create fl-tenant/fl-algorithm || true
+
+if [ "$1" == "dashboard" ]; then
+  # shellcheck disable=SC2155
+  export IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | tail -1 | awk '{ print $2 }')
+  export PULSAR_PROMETHEUS_URL=http://"$IP":9090
+
+  CSRF_TOKEN=$(curl http://127.0.0.1:7750/pulsar-manager/csrf-token)
+
+  curl \
     -H "X-XSRF-TOKEN: $CSRF_TOKEN" \
     -H "Cookie: XSRF-TOKEN=$CSRF_TOKEN;" \
     -H 'Content-Type: application/json' \
     -X PUT http://127.0.0.1:7750/pulsar-manager/users/superuser \
     -d '{"name": "admin", "password": "apachepulsar", "description": "test", "email": "admin@pulsar.apache.org"}'
 
-docker exec -it pulsar_pulsar_1 /pulsar/bin/pulsar-admin clusters update standalone --url http://"$IP":8080
+  $dc_exec -it pulsar /pulsar/bin/pulsar-admin clusters update standalone --url http://"$IP":8080
+fi
