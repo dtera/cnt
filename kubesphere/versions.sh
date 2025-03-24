675c4770a2721f3d46c09679e51aebd7b262b915
dtrea <45172737+dtera@users.noreply.github.com>
2025-03-24 19:54:44 +0800

    Update versions.sh

--- kubesphere/versions.sh
+++ kubesphere/versions.sh
@@ -4,10 +4,10 @@ CD=$(cd "$(dirname "$0")" || exit && pwd)
 cd "$CD" || exit
 
 # shellcheck disable=SC2034
-kk_ver=3.0.10
+kk_ver=3.1.7
 # shellcheck disable=SC2034
 ks_ver=3.4.0
 # shellcheck disable=SC2034
-k8s_ver=1.23.10
+k8s_ver=1.31.0
 # shellcheck disable=SC2034
 password=123
