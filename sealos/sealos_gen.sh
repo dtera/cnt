f4e6ec0a7a5450d655a7cf6b2bc62452ef82a48c
dtrea <45172737+dtera@users.noreply.github.com>
2025-03-25 14:36:04 +0800

    Update sealos_gen.sh

--- sealos/sealos_gen.sh
+++ sealos/sealos_gen.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # 生成包含 Kubernetes、Helm、Calico 的配置文件
-sealos gen registry.cn-shanghai.aliyuncs.com/labring/kubernetes:v1.31.0 \
-    registry.cn-shanghai.aliyuncs.com/labring/helm:v3.9.4 \
-    registry.cn-shanghai.aliyuncs.com/labring/calico:v3.24.1 \
+sealos gen labring/kubernetes:v1.25.0 labring/helm:v3.8.2 labring/calico:v3.24.1 \
     --masters 127.0.0.1 \
     --nodes 127.0.0.1 \
     --passwd 123456 \
     --port 22 \
-    --output Clusterfile
+    -o Clusterfile
