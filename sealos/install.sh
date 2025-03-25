85036085c8fdc7e2e8f21375184e3e20d56b27f7
dtrea <45172737+dtera@users.noreply.github.com>
2025-03-25 14:16:02 +0800

    Update install.sh

--- sealos/install.sh
+++ sealos/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 export PROXY_PREFIX=https://ghfast.top
-export SEALOS_VERSION=v5.0.1
+export SEALOS_VERSION=5.0.1
 export CLOUD_DOMAIN=www.wx.com  # [ip].nip.io
 
-curl -sfL "$PROXY_PREFIX"/https://raw.githubusercontent.com/labring/sealos/"$SEALOS_VERSION"/scripts/cloud/install.sh -o /tmp/install.sh  && \
-bash /tmp/install.sh   --cloud-version="$SEALOS_VERSION"   \
+[ -f /usr/bin/sealos ] || (wget https://github.com/labring/sealos/releases/download/v"$SEALOS_VERSION"/sealos_"$SEALOS_VERSION"_linux_amd64.rpm && yum install ./sealos_"$SEALOS_VERSION"_linux_amd64.rpm)
+
+curl -sfL "$PROXY_PREFIX"/https://raw.githubusercontent.com/labring/sealos/v"$SEALOS_VERSION"/scripts/cloud/install.sh -o /tmp/install.sh  && \
+bash /tmp/install.sh   --cloud-version=v"$SEALOS_VERSION"   \
 --image-registry=registry.cn-shanghai.aliyuncs.com --zh   \
 --proxy-prefix=“$PROXY_PREFIX” \
 --cloud-domain="$CLOUD_DOMAIN"
