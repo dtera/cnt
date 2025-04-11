0f5465d299ead0f161f19a8fe597f48b5336f8b8
dterazhao <dterazhao@tencent.com>
2025-04-11 15:47:14 +0800

    feat: install kubesphere

--- kubesphere/install.sh
+++ kubesphere/install.sh
@@ -4,5 +4,8 @@ CD=$(cd "$(dirname "$0")" || exit && pwd)
 cd "$CD" || exit
 
 export KKZONE=cn
+
+ks_ver=1.1.4
 # 如果无法访问 charts.kubesphere.io, 可将 charts.kubesphere.io 替换为 charts.kubesphere.com.cn
-helm upgrade --install -n kubesphere-system --create-namespace ks-core https://charts.kubesphere.com.cn/main/ks-core-1.1.4.tgz --debug --wait
+helm upgrade --install -n kubesphere-system --create-namespace ks-core \
+     https://charts.kubesphere.com.cn/main/ks-core-"$ks_ver".tgz --debug --wait
