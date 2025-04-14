9bf72ccb01f2c87f575efeffc417d4ccf27e3b8e
dterazhao <dterazhao@tencent.com>
2025-04-14 14:08:39 +0800

    feat: update install.sh

--- sealos/install.sh
+++ sealos/install.sh
@@ -13,8 +13,12 @@ if [[ $? != 0 ]]; then
   PROXY_PREFIX=${PROXY_PREFIX} sh -s ${VERSION} labring/sealos
 fi
 
-curl -sfL $PROXY_PREFIX/https://raw.githubusercontent.com/labring/sealos/v"$SEALOS_VERSION"/scripts/cloud/install.sh -o /tmp/install.sh && \
-  bash /tmp/install.sh --cloud-version=v"$SEALOS_VERSION"   \
-  --image-registry=registry.cn-shanghai.aliyuncs.com --zh   \
-  --proxy-prefix=$PROXY_PREFIX   \
+echo "PROXY_PREFIX: $PROXY_PREFIX"
+echo "SEALOS_VERSION: $SEALOS_VERSION"
+echo "CLOUD_DOMAIN: $CLOUD_DOMAIN"
+
+curl -sfL $PROXY_PREFIX/https://raw.githubusercontent.com/labring/sealos/v"$SEALOS_VERSION"/scripts/cloud/install.sh \
+  -o /tmp/install.sh && bash /tmp/install.sh --cloud-version=v"$SEALOS_VERSION" \
+  --image-registry=registry.cn-shanghai.aliyuncs.com --zh \
+  --proxy-prefix=$PROXY_PREFIX \
   --cloud-domain="$CLOUD_DOMAIN"
