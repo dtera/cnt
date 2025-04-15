d04264f17a0230d6993090b1ade1d4e75ff95a41
dtera <huster1005@gmail.com>
2025-04-15 12:40:31 +0800

    feat: update

--- run.sh
+++ run.sh
@@ -5,14 +5,26 @@ cd "$CD" || exit
 
 read -r -p "Please input password of the nodes to install k8s: " passwd
 
+if [[ "$passwd" == "" ]]; then
+  passwd="123456"
+fi
+
+opt="--cilium"
+if [[ "$1" == "seal_cloud" ]]; then
+  opt=""
+fi
 # install k8s
-sh "$CD"/sealos/k8s-install.sh -p "$passwd"
+sh "$CD"/sealos/k8s-install.sh $opt -p "$passwd"
 
-# install sealos cloud
-sh "$CD"/sealos/install.sh
+if [[ "$1" == "seal_cloud" ]]; then
+  # install sealos cloud
+  sh "$CD"/sealos/install.sh
+else
+  # install kubesphere
+  sh "$CD"/kubesphere/install.sh
 
-# install kubesphere
-# sh "$CD"/kubesphere/install.sh
+  # install dependencies by helm
+  sh "$CD"/sealos/helm-install-deps.sh
+fi
 
-# install dependencies by helm
-# sh "$CD"/sealos/helm-install-deps.sh
+
