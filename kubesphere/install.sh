601c0f37b8a90bc370235a03fa88a2de542abee0
dterazhao <dterazhao@tencent.com>
2026-06-16 16:09:30 +0800

    feat: 支持自定义 kubesphere 版本安装

--- kubesphere/install.sh
+++ kubesphere/install.sh
@@ -5,12 +5,17 @@ cd "$CD" || exit
 
 export KKZONE=cn
 
-# helm repo add kubesphere https://charts.kubesphere.com.cn/main
+helm repo add kubesphere https://charts.kubesphere.com.cn/main
 helm repo update
-helm upgrade --install -n kubesphere-system --create-namespace ks-core \
-  oci://hub.kubesphere.com.cn/kse/ks-core
 
-# ks_ver=1.1.4
-# 如果无法访问 charts.kubesphere.io, 可将 charts.kubesphere.io 替换为 charts.kubesphere.com.cn
-# helm upgrade --install -n kubesphere-system --create-namespace ks-core \
-#      https://charts.kubesphere.com.cn/main/ks-core-"$ks_ver".tgz --debug --wait
+ks_ver=${1:-latest}
+
+if [[ "$ks_ver" == "latest" ]]; then
+  helm upgrade --install -n kubesphere-system --create-namespace ks-core \
+    oci://hub.kubesphere.com.cn/kse/ks-core
+else
+  # ks_ver=1.1.4
+  # 如果无法访问 charts.kubesphere.io, 可将 charts.kubesphere.io 替换为 charts.kubesphere.com.cn
+  helm upgrade --install -n kubesphere-system --create-namespace ks-core \
+    https://charts.kubesphere.com.cn/main/ks-core-"$ks_ver".tgz --debug --wait
+fi
--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ read -r -p "Please input ip addresses of nodes to install k8s[127.0.0.1]: " node
 nodes=${nodes:-127.0.0.1}
 read -r -p "Please input password of the nodes to install k8s[123456]: " passwd
 passwd=${passwd:-123456}
+read -r -p "Please input the version of kubesphere[latest]: " ks_ver
+ks_ver=${ks_ver:-latest}
 
 opt="--cilium"
 if [[ "$1" == "seal_cloud" ]]; then
@@ -22,6 +24,7 @@ else
 fi
 
 # install k8s
+# shellcheck disable=SC2086
 sh "$CD"/sealos/k8s-install.sh $opt -p $passwd
 
 if [[ "$1" == "seal_cloud" ]]; then
@@ -29,7 +32,7 @@ if [[ "$1" == "seal_cloud" ]]; then
   sh "$CD"/sealos/install.sh
 else
   # install kubesphere
-  sh "$CD"/kubesphere/install.sh
+  sh "$CD"/kubesphere/install.sh "$ks_ver"
 
   # install dependencies by helm
   sh "$CD"/sealos/helm-install-deps.sh
